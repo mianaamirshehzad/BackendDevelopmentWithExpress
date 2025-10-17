bf78cfccd0990bbf25a09e1df55656c7fdf2f453
Aamir Shehzad <amir.shehzad@stackplus.ca>
2025-10-17 13:05:48 +0500

    Feat: GetAllProducts, UpdateProduct

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createProduct, deleteProduct } = require("../controllers/productController.js");
+const { createProduct, deleteProduct, getAllProducts, updateProduct } = require("../controllers/productController.js");
 
 const router = express.Router();
 
 router.post("/products", createProduct);
 router.delete("/product/:id", deleteProduct);
+router.get("/products", getAllProducts);
+router.put("/product/:id", updateProduct);
 
 module.exports = router;
