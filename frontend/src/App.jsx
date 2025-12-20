9c7fdfa27a1867806954aa0f91a4a2a649066f50
mianaamirshehzad <maamirshehzadpk1998@gmail.com>
2025-12-20 05:16:30 +0500

    Update: Signup page added in the flow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,13 @@ import AddBook from "./pages/AddBook";
 import EditBook from "./pages/EditBook";
 import BookDetails from "./pages/BookDetails";
 import "./App.css";
+import Signup from "./pages/authentication/Signup";
 
 function App() {
   return (
-    <div>
-      <nav className="navbar">
+    <div style={styles.container}>
+      <Signup />
+      {/* <nav className="navbar">
         <h2>Book Store</h2>
         <div>
           <Link to="/">Home</Link>
@@ -22,9 +24,19 @@ function App() {
         <Route path="/add" element={<AddBook />} />
         <Route path="/edit/:id" element={<EditBook />} />
         <Route path="/book/:id" element={<BookDetails />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
+};
+
+const styles = {
+    container: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        height: '100vh'
+    }
 }
 
 export default App;
